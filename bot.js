04d05f40009c33d3643821ccffdb7d1ce0a620bc
Chris <divinityforvalor@gmail.com>
2020-02-03 00:32:28 -0500

    Update to Discord Bot
    - Added example snippets to examples.json
    - Update refactor with RichEmbed messaging in bot.js

--- bot.js
+++ bot.js
@@ -5,43 +5,58 @@ const commandsData = require('./info/commands.json');
 const classData = require('./info/classes.json');
 const indexData = require('./info/index.json');
 const exampleData = require('./info/examples.json');
+const chrome = require('selenium-webdriver/chrome');
+const sleep = require('sleep-promise');
+const {Builder, By, Key, until, util} = require('selenium-webdriver');
 const bot = new Discord.Client();
 const token = process.env.TOKEN;
 const user = process.env.USER;
 const pass = process.env.PASS;
-const chrome = require('selenium-webdriver/chrome');
-const {Builder, By, Key, until, util} = require('selenium-webdriver');
-const sleep = require('sleep-promise');
+const prefix = process.env.PREFIX;
 const screen = {
     width: 640,
     height: 480
 };
-const loginURL = 'https://osbot.org/forum/login/';
-const homeURL = 'https://osbot.org/forum/';
 const scripts = {
     dragons: 898,
     agility: 975,
     vorkath: 1090
 };
+const loginURL = 'https://osbot.org/forum/login/';
+const homeURL = 'https://osbot.org/forum/';
 
-// Setup our trie search for class
+// Setup our trie search for class.json
 let classSearch = new Trie('name', {min: 3, ignoreCase: true});
 classSearch.addAll(classData);
-// Setup our trie search for index
+// Setup our trie search for index.json
 let indexSearch = new Trie('name', {min: 3, ignoreCase: true});
 indexSearch.addAll(indexData);
-// Setup our trie search for example
+// Setup our trie search for example.json
 let exampleSearch = new Trie('name', {min: 3, ignoreCase: true});
 exampleSearch.addAll(exampleData);
 
+/**
+ * Discord API login state
+ */
 bot.login(token);
 
+/**
+ * Discord API ready state
+ */
 bot.on('ready', () => {
     console.info(`Logged in as ${bot.user.tag}!`);
 });
 
+/**
+ * Discord API Message listener state
+ */
 bot.on('message', msg => {
+    // If the message author was a bot we ignore it
     if (msg.author.bot)
+        return;
+
+    // If the prefix was not found we ignore the message on the server
+    if (msg.content.charAt(0) !== prefix)
         return;
 
     let msgContent = msg.content;
@@ -51,12 +66,15 @@ bot.on('message', msg => {
     if (msgContent === '!commands') {
         msg.reply('Here are a list of options available for this bot!\n')
             .then(() => {
-                let commandString = '';
+                let embed = new Discord.RichEmbed()
+                    .setTitle('Command List')
+                    .setColor('#f1c199')
+                    .setTimestamp();
                 for (let i = 0; i < commandsData.commands.length; i++) {
                     let command = commandsData.commands[i];
-                    commandString += '```\n' + command['name'] + ' - ' + command['desc'] + '\n```\n';
+                    embed.addField(command['name'], command['desc']);
                 }
-                msg.channel.send(commandString).then(() =>
+                msg.channel.send(embed).then(() =>
                     console.log(`Replied to user ${msg.author.username} with available commands!`)
                 );
             });
@@ -77,14 +95,18 @@ bot.on('message', msg => {
             }
 
             let lengthOfSearch = search.length;
-            let classString = lengthOfSearch > 1 ? 'Here are some classes from your search:\n' : 'Found a class from your search:\n';
+            let classTitle = lengthOfSearch > 1 ? 'Here are some classes from your search:' : 'Found a class from your search:';
+            let embed = new Discord.RichEmbed()
+                .setTitle(classTitle)
+                .setColor('#a0e6f1')
+                .setTimestamp();
 
             for (let i = 0; i < search.length; i++) {
                 let data = search[i];
-                classString += `${data['name']} class can be found at ${data['url']}\n\n`
+                embed.addField(data['name'], data['url']);
             }
 
-            msg.reply(classString)
+            msg.reply(embed)
                 .then(() => console.log(`Successfully found data for search: ` + fullCommand[1]));
         } else {
             msg.reply('Try providing a search term in your command. <!class bank>')
@@ -100,17 +122,21 @@ bot.on('message', msg => {
             }
 
             let lengthOfSearch = search.length;
-            let indexString = lengthOfSearch > 1 ? 'Here are some results from your search:\n' : 'Found a result from your search:\n';
+            let indexTitle = lengthOfSearch > 1 ? 'Here are some results from your search:' : 'Found a result from your search:';
+            let embed = new Discord.RichEmbed()
+                .setTitle(indexTitle)
+                .setColor('#f16192')
+                .setTimestamp();
 
             for (let i = 0; i < 10; i++) {
                 let data = search[i];
                 // Incase there are less than 10 results.
                 if (!data)
                     continue;
-                indexString += `${data['name']}\n${data['desc']}\n${data['url']}\n\n`
+                embed.addField(data['name'], data['desc'] + '\n' + data['url']);
             }
 
-            msg.reply(indexString)
+            msg.reply(embed)
                 .then(() => console.log(`Successfully found data for search: ` + fullCommand[1]));
         } else {
             msg.reply('Try providing a search term in your command. <!index open>')
@@ -126,19 +152,21 @@ bot.on('message', msg => {
             }
 
             let lengthOfSearch = search.length;
-            let indexString = lengthOfSearch > 1 ? 'Here are some snippets from your search:\n' : 'Found a snippet from your search:\n```';
+            let exampleTitle = lengthOfSearch > 1 ? 'Here are some snippets from your search:' : 'Found a snippet from your search:';
+            let embed = new Discord.RichEmbed()
+                .setTitle(exampleTitle)
+                .setColor('#9c8ff1')
+                .setTimestamp();
 
             for (let i = 0; i < 10; i++) {
                 let data = search[i];
                 // Incase there are less than 10 results.
                 if (!data)
                     continue;
-                indexString += `${data['name']}\n${data['code']}\n\n`
+                embed.addField(data['name'], '```' + data['code'] + '```');
             }
 
-            indexString += '```';
-
-            msg.reply(indexString)
+            msg.reply(embed)
                 .then(() => console.log(`Successfully found data for search: ` + fullCommand[1]));
         } else {
             msg.reply('Please provide the command in the proper format.\n For example,\n!example banking')
@@ -174,6 +202,10 @@ bot.on('message', msg => {
     }
 });
 
+/**
+ * @param name - the name of the script
+ * @returns {number} - the id of the script by name
+ */
 function getScriptIDByName(name) {
     for (let key in scripts) {
         let value = scripts[key];
@@ -184,6 +216,10 @@ function getScriptIDByName(name) {
     return -1;
 }
 
+/**
+ * @param authURL - created and passed
+ * @returns {Promise<void>} - If the promise was resolved then it will trigger the auth reply message.
+ */
 async function auth(authURL) {
     let driver = await new Builder()
         .forBrowser('chrome')
